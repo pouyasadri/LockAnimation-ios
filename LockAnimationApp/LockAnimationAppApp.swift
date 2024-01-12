094b70b64eac25b3edc3fbe5b9691e2fe7f242d1
pouyasadri <pouyapaghandeh78@gmail.com>
2024-01-12 21:21:42 +0100

    Initial Commit

---
+++ LockAnimationApp/LockAnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockAnimationAppApp.swift
+//  LockAnimationApp
+//
+//  Created by Pouya Sadri on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LockAnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
